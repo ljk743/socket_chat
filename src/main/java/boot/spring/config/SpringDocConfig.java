19dd69cc31c4bab9f96daaeb63c467d53e3b6d2f
ljk743 <3309411542@qq.com>
2024-03-17 23:59:42 +0000

    update Swagger to manage our APIs

---
+++ src/main/java/boot/spring/config/SpringDocConfig.java
@@ -0,0 +1,18 @@
+package boot.spring.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("socket-chat")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
